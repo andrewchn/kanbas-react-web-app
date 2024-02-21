b1bb61c51da1e5b0de5d48bf97a0348298ffdd0c
Andrew <ajchin01@gmail.com>
2024-02-20 19:22:41 -0500

    a3

---
+++ src/Kanbas/index.tsx
@@ -0,0 +1,21 @@
+import { Routes, Route, Navigate } from "react-router";
+import Dashboard from "./Dashboard";
+import KanbasNavigation from "./Navigation";
+
+function Kanbas() {
+  return (
+    <div className="d-flex">
+      <KanbasNavigation />
+
+      <div style={{ flexGrow: 1 }}>
+        <Routes>
+          <Route path="/" element={<Navigate to="Dashboard" />} />
+          <Route path="Account" element={<h1>Account</h1>} />
+          <Route path="Dashboard" element={<Dashboard />} />
+          <Route path="Courses/*" element={<h1>Courses</h1>} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+export default Kanbas;
